247ac09782e7db49be466e918c502234f966f2bb
Mawathilde <dev@mawathilde.fr>
2025-03-19 10:31:27 +0100

    Initial commit

---
+++ src/client/java/fr/mawathilde/DemoFabricClient.java
@@ -0,0 +1,10 @@
+package fr.mawathilde;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DemoFabricClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
